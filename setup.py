c9c916acdadfc6822a09549dedca52bb06019853
microprediction <peter.d.cotton@jpmorgan.com>
2020-09-22 21:13:02 -0400

    blah

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     test_suite='pytest',
     tests_require=['pytest'],
     include_package_data=True,
-    install_requires=["pandas","numpy","pytest","python-dateutil","statsmodels","microfilter","optuna", "sklearn","microconventions", "deap"],
+    install_requires=["pandas","numpy","pytest","python-dateutil","statsmodels","microfilter","optuna", "sklearn","microconventions", "deap","wheel"],
     entry_points={
         "console_scripts": [
             "tuneup=tuneup.__main__:main",
