7d5762792f9ea433c70b7cc68c01b21b7a60a7e9
Peter Cotton <peter.cotton@microprediction.com>
2020-10-23 14:39:10 -0400

    shgo

--- tuneup/ndimsolvers/shgocube.py
+++ tuneup/ndimsolvers/shgocube.py
@@ -7,7 +7,7 @@ def shgo_cube(objective,scale, n_trials, n_dim):
     def _objective(x):
         return objective(list(x))[0]
 
-    result = shgo(_objective, bounds, options={'maxfev':n_trials}, sampling_method='sobol')
+    result = shgo(_objective, bounds, options={'maxfev':n_trials,'minimize_every_iter':False}, sampling_method='sobol')
     return result.fun
 
 
